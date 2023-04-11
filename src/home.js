fdc99c4709384ee464df2201fcb355f0353c156f
po-tent <130344415+po-tent@users.noreply.github.com>
2023-04-10 20:02:30 -0400

    Add files via upload

---
+++ src/home.js
@@ -0,0 +1,34 @@
+import nav from './nav';
+import footer from './footer';
+
+let home = () => {
+  let content = document.getElementById('content');
+  content.classList.add('home');
+
+  if (!document.querySelector('nav')) {
+    let navbar = nav()
+    content.appendChild(navbar);
+  }
+  let home = document.getElementById('home');
+  home.classList.add('active');
+
+  let container = document.createElement('div');
+  container.classList.add('container');
+  let header = document.createElement('header');
+  let headline = document.createElement('h1');
+  headline.innerHTML = 'Tobago & Tripoli';
+  header.appendChild(headline);
+  container.appendChild(header);
+
+  let tagline = document.createElement('p');
+  tagline.innerHTML = 'Where Bougie Meets Broke';
+  header.appendChild(tagline);
+
+  content.appendChild(container);
+
+  if (!document.querySelector('footer')) {
+    footer();
+  }
+}
+
+export default home
